8e16fcefe1ba4108a1c88565f6e77f4effc406b0
Priti Gosavi <82075813+pritig5@users.noreply.github.com>
2024-07-28 02:25:44 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const sr = ScrollReveal({
+    origin: 'right',
+    distance:  '80px',
+    duration: 2500,
+    delay: 300,
+})
+
+sr.reveal('.home__data')
+sr.reveal('.home__social, .home__button')
+sr.reveal('.home__perfil', {origin: 'left'})
+sr.reveal('.section__title', {origin: 'top'} )
+// sr.reveal('.work__container', {origin: 'bottom'})
+sr.reveal('.contact__button', {origin: 'bottom'})
+sr.reveal('.services', {origin: 'bottom'})
+sr.reveal('.about__perfil', {origin: 'left'})
+sr.reveal('.about__content', {origin: 'right'})
+// sr.reveal('.about__info', {delay: 400})
+// sr.reveal('.about__skills', {origin: 'bottom'})
+sr.reveal('.contact__page', {origin: 'bottom'}) 
+sr.reveal('.wrapper', {origin: 'bottom'})
